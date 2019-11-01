2d81f32963773ab73001d98bd65d63958f83e2e8
cnyangx <yangxiuqian95@outlook.com>
2019-11-01 14:59:36 +0800

    laoyang

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import App from './App.vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import './plugins/element.js'
+Vue.config.productionTip = false
+/* 
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
+ */
+Vue.use(ElementUI);
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+});
